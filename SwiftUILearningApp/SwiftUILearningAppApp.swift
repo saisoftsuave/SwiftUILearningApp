02edac245171c1c8ac8c490888dc382e3a987bd5
saisoftsuave <sai.babu@softsuave.org>
2025-03-13 15:17:09 +0530

    Initial Commit

---
+++ SwiftUILearningApp/SwiftUILearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningAppApp.swift
+//  SwiftUILearningApp
+//
+//  Created by softsuave on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
